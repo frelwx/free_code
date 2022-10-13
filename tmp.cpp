a46a84b446bd976f257b3585bfd7758758a6c830
frelex <1228145131@qq.com>
2022-10-14 01:08:51 +0800

    ignore && tmp

--- tmp.cpp
+++ tmp.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 int main(){
     int a;
-    int b = scanf("%d", &a);
+    int b = 3;
     std::cout << a << ' ' << b << endl;
     int x = 2;
     int y = 4;
     int z = 10;
     int k = 6;
+    return 0;
 }
